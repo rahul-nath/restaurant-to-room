8e361636626209bd873a07ba8760a5b3beb9c767
Rahul Nath <rn2@alumni.williams.edu>
2015-08-13 18:53:47 +0000

    now pushing to github

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var userService = require('../services/user-service');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-
-// a get request for users/views (because /user is the relative root in this file)
-router.get('/views', function(req, res, next) {
-  res.send('user view');
+router.get('/create', function(req, res, next) {
+  var vm = {title: 'Create an account'};
+  res.render('users/create', vm);
 });
+
+router.post('/create', function(req, res, next) {
+    userService.addUser(req.body, function(err){
+        if(err){
+            var vm = {
+                title: 'Create an account',
+                input: req.body,
+                error: 'something went wrong'
+            };
+            delete vm.input.password;
+            return res.render('users/create', vm);
+        }
+        res.redirect('/orders');
+    });
+
+
 
 module.exports = router;
